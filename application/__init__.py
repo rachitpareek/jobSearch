f548d35de4d2600d21204f0c2ee1b5dc14b07420
Rachit Pareek <rachitpareek@berkeley.edu>
2020-01-06 10:41:36 -0800

    added photo on homepage and footer

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates/static')
 app.config.from_object(Config)
 
 bootstrap = Bootstrap(app)
