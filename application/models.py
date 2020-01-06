b758337e38fe0b0ae1aa0bb48b98feda824cfbef
Rachit Pareek <rachitpareek@berkeley.edu>
2020-01-05 20:00:52 -0800

    cleaning repo, then frontend time

--- application/models.py
+++ application/models.py
@@ -1,13 +1,26 @@
-from application import db
+from application import db, login
 from datetime import datetime
+from flask_login import UserMixin
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)   
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
